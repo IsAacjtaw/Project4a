a0cf731e2a20074116dec3fc68169608f2d673d3
kelee <kelee@163.11.237.99>
2024-12-04 16:54:26 -0500

    Day 4

--- src/tomasulogui/FunctionalUnit.java
+++ src/tomasulogui/FunctionalUnit.java
@@ -1,31 +1,41 @@
 package tomasulogui;
 
 public abstract class FunctionalUnit {
-  PipelineSimulator simulator;
-  ReservationStation[] stations = new ReservationStation[2];
-  
-  public FunctionalUnit(PipelineSimulator sim) {
-    simulator = sim;
-    
-  }
 
- 
-  public void squashAll() {
-    // todo fill in
-  }
+    PipelineSimulator simulator;
+    ReservationStation[] stations = new ReservationStation[2];
+    int currExCycle;
 
-  public abstract int calculateResult(int station);
+    public FunctionalUnit(PipelineSimulator sim) {
+        simulator = sim;
 
-  public abstract int getExecCycles();
+    }
 
-  public void execCycle(CDB cdb) {
-    //todo - start executing, ask for CDB, etc.
-  }
+    public void squashAll() {
+        // todo fill in
+    }
 
+    public abstract int calculateResult(int station);
 
+    public abstract int getExecCycles();
 
-  public void acceptIssue(IssuedInst inst) {
-  // todo - fill in reservation station (if available) with data from inst
-  }
+    public void execCycle(CDB cdb) {
+        //todo - start executing, ask for CDB, etc.
+        if (stations[0].stat == ReservationStation.Status.PROCESSING) {
+            if (currExCycle++ == getExecCycles()) {
+                // station is done computing
+            }
+        }
+        if (stations[1].stat == ReservationStation.Status.PROCESSING) {
+            if (currExCycle++ == getExecCycles()) {
+                // station is done computing
+            }
+        }
+
+    }
+
+    public void acceptIssue(IssuedInst inst) {
+        // todo - fill in reservation station (if available) with data from inst
+    }
 
 }
