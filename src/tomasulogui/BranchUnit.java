372da95f923d4665ded0be262c440965aa6c365a
Isaac <itawney@cedarville.edu>
2024-12-10 14:47:48 -0500

    branchunit

--- src/tomasulogui/BranchUnit.java
+++ src/tomasulogui/BranchUnit.java
@@ -1,5 +1,10 @@
 package tomasulogui;
 
+import static tomasulogui.IssuedInst.INST_TYPE.BEQ;
+import static tomasulogui.IssuedInst.INST_TYPE.BLEZ;
+import static tomasulogui.IssuedInst.INST_TYPE.BLTZ;
+import static tomasulogui.IssuedInst.INST_TYPE.BNE;
+
 public class BranchUnit extends FunctionalUnit {
     
     public static final int EXEC_CYCLES = 1;
@@ -13,8 +18,43 @@ public class BranchUnit extends FunctionalUnit {
     public int calculateResult(int station) {
         int op1 = stations[station].data1;
         int op2 = stations[station].data2;
-        if (op1 != op2) {
-            return 1;
+        IssuedInst.INST_TYPE instructype = stations[station].getFunction();
+        
+        switch(instructype){
+            case BEQ:
+                if(op1 == op2){
+                    return 1;
+                }
+                break;
+            case BNE:
+                if(op1 != op2){
+                    return 1;
+                }
+                break;
+            case BLTZ:
+                if(op1 < op2){
+                    return 1;
+                }
+                break;
+            case BLEZ:
+                if(op1 <= op2){
+                    return 1;
+                }
+                break;
+            case BGEZ:
+                if(op1 > op2){
+                    return 1;
+                }
+                break;
+            case BGTZ:
+                if(op1 >= op2){
+                    return 1;
+                }
+                break;
+            default:
+                System.out.println("Branch unit default case no branch type?"
+                        + " shouldn't be here I think");
+                return -3;
         }
         return 0;
     }
